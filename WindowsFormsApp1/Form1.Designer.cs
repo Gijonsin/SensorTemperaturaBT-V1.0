11599cd5d2086046dc4a1054b31b416c46b4539a
JOSE EDUARDO GIJON <alu.21130883@correo.itlalaguna.edu.mx>
2024-09-28 04:16:27 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_Puertos = new System.Windows.Forms.ComboBox();
+            this.btn_Conectar = new System.Windows.Forms.Button();
+            this.btn_Actualizar = new System.Windows.Forms.Button();
+            this.lab_Temperatura = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SELECCIONA UN PUERTO";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cb_Puertos
+            // 
+            this.cb_Puertos.FormattingEnabled = true;
+            this.cb_Puertos.Location = new System.Drawing.Point(59, 54);
+            this.cb_Puertos.Name = "cb_Puertos";
+            this.cb_Puertos.Size = new System.Drawing.Size(274, 21);
+            this.cb_Puertos.TabIndex = 2;
+            // 
+            // btn_Conectar
+            // 
+            this.btn_Conectar.Location = new System.Drawing.Point(349, 54);
+            this.btn_Conectar.Name = "btn_Conectar";
+            this.btn_Conectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Conectar.TabIndex = 3;
+            this.btn_Conectar.Text = "CONECTAR";
+            this.btn_Conectar.UseVisualStyleBackColor = true;
+            this.btn_Conectar.Click += new System.EventHandler(this.btn_Conectar_Click);
+            // 
+            // btn_Actualizar
+            // 
+            this.btn_Actualizar.Location = new System.Drawing.Point(430, 54);
+            this.btn_Actualizar.Name = "btn_Actualizar";
+            this.btn_Actualizar.Size = new System.Drawing.Size(91, 23);
+            this.btn_Actualizar.TabIndex = 4;
+            this.btn_Actualizar.Text = "ACTUALIZAR";
+            this.btn_Actualizar.UseVisualStyleBackColor = true;
+            this.btn_Actualizar.Click += new System.EventHandler(this.btn_Actualizar_Click);
+            // 
+            // lab_Temperatura
+            // 
+            this.lab_Temperatura.AutoSize = true;
+            this.lab_Temperatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_Temperatura.Location = new System.Drawing.Point(59, 135);
+            this.lab_Temperatura.Name = "lab_Temperatura";
+            this.lab_Temperatura.Size = new System.Drawing.Size(428, 55);
+            this.lab_Temperatura.TabIndex = 5;
+            this.lab_Temperatura.Text = "NO CONECTADO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 310);
+            this.Controls.Add(this.lab_Temperatura);
+            this.Controls.Add(this.btn_Actualizar);
+            this.Controls.Add(this.btn_Conectar);
+            this.Controls.Add(this.cb_Puertos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MONITOR DE TEMPERATURA BLUETOOTH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_Puertos;
+        private System.Windows.Forms.Button btn_Conectar;
+        private System.Windows.Forms.Button btn_Actualizar;
+        private System.Windows.Forms.Label lab_Temperatura;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            cb_Puertos.DataSource = puertos;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort actualSP = (SerialPort)sender;
+            string inData = actualSP.ReadLine();
+            lab_Temperatura.Invoke(new MethodInvoker(delegate
+            {
+                lab_Temperatura.Text = "Temperatura: " + inData + " °C";
+            }));
+        }
+
+        private void btn_Conectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sp.PortName = cb_Puertos.Text;
+                sp.BaudRate = 9600; // Asegúrate de que la velocidad en baudios coincida con la del módulo Bluetooth
+                sp.Open();
+                sp.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+                MessageBox.Show("Conectado al puerto " + sp.PortName);
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btn_Actualizar_Click(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            cb_Puertos.DataSource = puertos;
+        }
+    }
+    
+}
